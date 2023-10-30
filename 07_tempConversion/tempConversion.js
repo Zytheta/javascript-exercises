00ebd3d459d33f61c1278e77bb8cf3fc08043839
Zytheta <112493405+Zytheta@users.noreply.github.com>
2023-10-30 13:23:24 +0900

    Added next project

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function(tempFahrenheit) {
+  let convertedCelsius = 0;
+  convertedCelsius = ((tempFahrenheit - 32) * 5 / 9);
+  const convert = (convertedCelsius) => (Math.round(convertedCelsius * 10)) / 10;
+  return convert(convertedCelsius);
+  };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelsius) {
+  let convertedFahrenheit = 0;
+  convertedFahrenheit = ((tempCelsius * 9 / 5 + 32))
+  const revert = (convertedFahrenheit) => (Math.round(convertedFahrenheit * 10)) / 10;
+  return revert(convertedFahrenheit);
 };
 
 // Do not edit below this line
