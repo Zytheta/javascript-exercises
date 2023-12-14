9f94ed0256bd20386f6226b153aed896c85de425
Zytheta <112493405+Zytheta@users.noreply.github.com>
2023-12-14 13:19:02 +0900

    Adding stuff that I did a while ago

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    const cleanText = text.replace(/[^\w]|_/g, '').toLowerCase();
+    const characters = [...cleanText];
+    const textCharacters = characters.join();
+    console.log(textCharacters);
+    const palindromeCharacters = [...cleanText].reverse();
+    const palindrome = palindromeCharacters.join();
+    console.log(palindrome);
+    if (textCharacters === palindrome) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(lengthOfFibonacci) {
+    const intChange = parseInt(lengthOfFibonacci);
+    if (intChange === 0) {
+        return 0;
+    } else if (intChange === 1) {
+        return 1;
+    } else if (intChange < 0) {
+        return "OOPS";
+    } else {
+        let previous = 0;
+        let current = 1;
+        let result = 0;
+        for (let i = 2; i <= intChange; i++) {
+            result = previous + current;
+            previous = current;
+            current = result;
+        }
+        return result
+    }
 };
 
 // Do not edit below this line
